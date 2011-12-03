0def8bb1bfd3732b5186a00691c2f148358db22a
Tammy Cravit <tcravit@taylored-software.com>
2011-12-02 16:57:43 -0800

    Removed Utility from the autoloaded classes

--- lib/tcravit_ruby_lib.rb
+++ lib/tcravit_ruby_lib.rb
@@ -3,7 +3,6 @@ $:.unshift File.dirname(__FILE__)
 require "tcravit_ruby_lib/version"
 require 'tcravit_ruby_lib/config_searcher'
 require 'tcravit_ruby_lib/app_config'
-require 'tcravit_ruby_lib/utility'
 
 module TcravitRubyLib # :nodoc:
 end
