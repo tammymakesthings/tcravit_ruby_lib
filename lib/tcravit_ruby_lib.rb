147c04d9870722b55180f29ed5dad2ee09608037
Tammy Cravit <tammycravit@me.com>
2018-01-31 12:57:17 -0500

    Updated library loader class to autoload lib directory

--- lib/tcravit_ruby_lib.rb
+++ lib/tcravit_ruby_lib.rb
@@ -1,9 +1,10 @@
 $:.unshift File.dirname(__FILE__)
 
 require "tcravit_ruby_lib/version"
-require 'tcravit_ruby_lib/config_searcher'
-require 'tcravit_ruby_lib/app_config'
-require 'tcravit_ruby_lib/configurable'
+
+Dir[File.join(File.dirname(__FILE__), "tcravit_ruby_lib", "*.rb")].each do |f|
+	require File.join("tcravit_ruby_lib", File.basename(f)) unless File.basename(f) == "version.rb"
+end
 
 module TcravitRubyLib # :nodoc:
 end
---
+++ spec/tcravit_ruby_lib/global_spec.rb
@@ -0,0 +1,5 @@
+describe "TcravitRubyLib" do
+	it "should define a version constant" do
+		TcravitRubyLib::VERSION.should_not be_nil
+	end
+end
