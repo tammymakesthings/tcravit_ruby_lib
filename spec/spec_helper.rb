6b068bd696ac0a562ef43e0ef8316510132f9cd2
Tammy Cravit <tammycravit@me.com>
2018-02-01 19:16:54 -0500

    Update specs to new rspec expectations syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,9 @@ require 'rspec'
 require 'tcravit_ruby_lib'
 
 RSpec.configure do |config|
-#  config.color_enabled = true
   config.formatter     = 'documentation'
-#	config.expect_with(:rspec) { |c| c.syntax = :should }
+	config.expect_with :rspec do |c|
+		c.syntax = :expect
+		c.on_potential_false_positives = :nothing
+	end
 end
-
-RSpec::Expectations.configuration.on_potential_false_positives = :nothing
