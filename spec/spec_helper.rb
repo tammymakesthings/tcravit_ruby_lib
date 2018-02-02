2871f154c847f6ba9ffa9bc435eda8afa3ae3a45
Tammy Cravit <tammycravit@me.com>
2018-02-01 19:32:07 -0500

    Changed rspec output format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@ require 'rspec'
 require 'tcravit_ruby_lib'
 
 RSpec.configure do |config|
-  config.formatter     = 'progress'
+#  config.formatter     = 'progress'
+  config.formatter     = 'documentation'
   config.expect_with :rspec do |c|
     c.syntax = :expect
     c.on_potential_false_positives = :nothing
