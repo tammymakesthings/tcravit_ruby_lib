30130219885dfaf72511b4d6c2ba31a4808561aa
Tammy Cravit <tammycravit@me.com>
2018-02-04 19:46:48 -0500

    Updated documentation comments for spec files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,36 @@
+############################################################################
+# TcravitRubyLib: Random useful stuff for Ruby programming.
+#
+# File        : spec_helper.rb
+# Decription  : RSpec configuration and global helper functions for testing
+############################################################################
+#  Copyright 2011-2018, Tammy Cravit.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+############################################################################
+
 require 'rspec'
 require 'tcravit_ruby_lib'
+
+# A quick-and-dirty method to load RSpec helper files in spec/support
+def require_helper_named(helper_name)
+  require "#{File.join(File.dirname(__FILE__), "support", "#{helper_name}.rb")}"
+end
+
+# A quick-and-dirty method to load RSpec matchers in spec/support/matchers
+def require_custom_matcher_named(matcher_name)
+  require "#{File.join(File.dirname(__FILE__), "support", "matchers", "#{matcher_name}_matcher.rb")}"
+end
 
 RSpec.configure do |config|
 #  config.formatter     = 'progress'
--- spec/version_bump_rake_tasks_spec.rb
+++ spec/version_bump_rake_tasks_spec.rb
@@ -1,9 +1,32 @@
+############################################################################
+# TcravitRubyLib: Random useful stuff for Ruby programming.
+#
+# File        : version_bump_rake_tasks_spec.rb
+# Specs for   : version:bump rake tasks in tcravit_ruby_lib/rake_tasks
+############################################################################
+#  Copyright 2011-2018, Tammy Cravit.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+############################################################################
+
 require 'spec_helper'
 require "rake"
 require "tcravit_ruby_lib/rake_tasks"
 require "fantaskspec"
 
-require "#{File.join(File.dirname(__FILE__), "support", "matchers", "version_bump_matchers.rb")}"
+require_custom_matcher_named("be_a_rake_task_named")
+require_custom_matcher_named("be_a_valid_gem_version_file_for")
+require_custom_matcher_named("declare_the_gem_version_to_be")
 
 TEST_VERSION_FILE = "/tmp/bump_ver.out"
 
